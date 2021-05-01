7d395a5fdd7f47491c0f465f6a6613890ef0f158
ahmed4end <as4end@gmail.com>
2021-05-01 03:06:24 +0200

    patched and added vars.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+"use luck";
+
 /*
 
 // Diable F5 Button
@@ -32,7 +34,7 @@ document.onkeydown = function(e) {
 
 */
 
-
+//preloader config
 $(window).load(function() {
     $(".preloader").delay(1000).fadeOut("smooth");
 });
@@ -46,17 +48,6 @@ function text(txt){ // [DEV] remove it - no need for it.
     return `<div id="textCell">${txt}</div>`;
 };
 
-// id_col_icon vars
-var dynamicT = 'هذه المسئلة ديناميكة'
-var staticT = 'هذه المسئلة إستاتيكية'
-//
-
-
-const icon_book = `<svg width="25" height="25" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
-<path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-</svg>`
-
-const icon_search = `<svg width="24" height="24" viewBox="0 0 48 48" fill="rgba(22, 24, 35, 0.34)" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M22 10C15.3726 10 10 15.3726 10 22C10 28.6274 15.3726 34 22 34C28.6274 34 34 28.6274 34 22C34 15.3726 28.6274 10 22 10ZM6 22C6 13.1634 13.1634 6 22 6C30.8366 6 38 13.1634 38 22C38 25.6974 36.7458 29.1019 34.6397 31.8113L43.3809 40.5565C43.7712 40.947 43.7712 41.5801 43.3807 41.9705L41.9665 43.3847C41.5759 43.7753 40.9426 43.7752 40.5521 43.3846L31.8113 34.6397C29.1019 36.7458 25.6974 38 22 38C13.1634 38 6 30.8366 6 22Z"></path></svg>`
 
 //swal - welcome message
 function sleep(ms) {
@@ -83,18 +74,16 @@ function table_id_parser(data, type ){
         switch(data[0]){
             case 'd':
                 icon='dynamic_lamp';
-                tooltip = dynamicT;
                 break;
             case 's':
                 icon='static_lamp';
-                tooltip= staticT;
                 break;
         }
     } 
     return  `<div style="writing-mode: tb-rl;display:inline-block;">
                 <div style="text-align: center;margin-bottom:5px;" class="${icon}">
             </div>
-            <div style="display: inline-block;" id="table1Id">${data[1]}</div>`
+            <div style="display: inline-block;color:grey;" id="table1Id">${data[1]}</div>`
 }
 
 function table_col_last(data, type){
@@ -102,7 +91,6 @@ function table_col_last(data, type){
 }
 
 function table_col3(arr) {
-
     // arr = [unit, lesson, page, prob, node]
     var res = []
     if (arr[0]) {res.push(`و${arr[0]}`)};
@@ -114,9 +102,10 @@ function table_col3(arr) {
 }
 
 
-
 $(document).ready(function(){
-
+    // tooltips config.
+    tippy.setDefaultProps({delay: [100, 50]});
+    tippy('[data-tippy-content]');
     // swal var.
     var swal_modals = []
     // swal toastr.
@@ -137,24 +126,11 @@ $(document).ready(function(){
     var increament_var = 1
     const audio = new Audio("assets/btn2.wav"); 
 
+
     //table 1
     var table1 = $('#table1').DataTable( {
-        "autoWidth":false,
-        "lengthChange":true,
-        "paging":true,
-        "pageLength": 25,
-        "lengthMenu": [ 25, 50, 100, 200, 800 ],
-        "bPaginate":false,
-        "order": [],
-        "bAutoWidth": false,
-        "bInfo":true,
-        "dom": '<"top"><f><t><"#margin5px"><"clear"><"footerContainer"lp>',
-        rowReorder:false,
+        
         data: dataSet,
-        fixedHeader: {
-            header: false,
-            footer: false
-        },
         columns: [
             { title: "i."},
             { title: "id", render: table_id_parser},
@@ -167,30 +143,9 @@ $(document).ready(function(){
             { title: "وصف"},
             { title: "إعدادات", render: table_col_last, width:'20%'}
         ],
-        "columnDefs": [{
-            "searchable": true,
-            "orderable": false,
-            "targets": 0,
-            "className": "dt-center", 
-            "targets": "_all"
-        }], 
-        "language": {
-            "sLengthMenu": "أظهر _MENU_ مدخلات",
-            "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
-            "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
-            "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
-            "sInfoPostFix": "",
-            "sSearch": "ابحث:",
-            "search": "_INPUT_"+`<div class='search-icon'>${icon_search}</div>`,
-            "sUrl": "",
-            "oPaginate": {
-                "sFirst": "الأول",
-                "sPrevious": "السابق",
-                "sNext": "التالي",
-                "sLast": "الأخير"
-            }
-        },
-
+        
+        ...table1_args, // vars.js
+        
         //table column 0 filter.
         initComplete: function () {
             this.api().columns().every( function () {
@@ -210,21 +165,14 @@ $(document).ready(function(){
                 } );
             } );
         },
+
     });
 
     //add placeholder to filter.
-    $('#table1_filter input').attr('placeholder', 'أبحث عن ...');
+    $('#table1_filter input').attr('placeholder', 'البحث');
 
     //table pages
     var table1_nodes = table1.rows().nodes();
-
-    // tooltip - table 1
-    tippy(`.dynamic_lamp`, {
-        content: dynamicT,
-    });
-    tippy(`.static_lamp`, {
-        content: staticT,
-    });
 
     //lazy loader - table 1.
     const table1_images = $('.lozad', table1_nodes);
@@ -251,13 +199,7 @@ $(document).ready(function(){
 
     //table 2
     var table2 = $('#table2').DataTable({
-        rowReorder:true,
-        "lengthChange":false,
-        "bFilter": false,
-        "bLengthChange": false,
-        "bAutoWidth": false,
-        "bPaginate": false,
-        "bInfo":false,
+    
         columns: [
             { title: "i." },
             { title: "id" },
@@ -267,13 +209,9 @@ $(document).ready(function(){
             { title: "وصف" },
             { title: "إعدادات", width:'20%' }
         ],
-        "columnDefs": [{
-            "searchable": true,
-            "orderable": false,
-            "targets": 0,
-            "className": "dt-center reorder", //row reorder - class 'reorder' changes cursor icon on hover.
-            "targets": "_all"
-        }],
+
+        ...table2_args, // vars.js
+        
     });
 
     // reorder column 1 indexes when swaping any rows.
@@ -622,11 +560,6 @@ $(document).ready(function(){
             swal_modals.push({title: 'تحذير', html: con, timer:15000})
         }
     }
-
-    // tooltips config.
-    tippy.setDefaultProps({delay: [100, 50]});
-    tippy('[data-tippy-content]');
-
 }); 
 
 
@@ -653,9 +586,9 @@ $('.tabs li').on('click', function(tab){
     $(`.tabs-panel[data-index="${index}"]`).addClass("active");
 });
 
-const fd = `
+const about_modal = `
 <small>
-هذه الإصدار من البرنامج يحتوى على كتاب المعاصر الصف الأول الثانوى الترم الأول
+هذه الإصدار من البرنامج يحتوى على<br>كتاب المعاصر الصف الأول الثانوى الترم الأول
 </small>
 <fieldset>
     <legend>
@@ -684,7 +617,7 @@ $('.about-btn').on('click', function(){
     Swal.fire({
         title: "<h1>Mathar 1.0</h1>",
         icon: 'info',
-        html: "The teacher's tool to innovate.<br>"+fd,
+        html: "The teacher's tool to innovate.<br>"+about_modal,
         showConfirmButton: false,
         showCloseButton: true
     })
---
+++ assets/js/vars.js
@@ -0,0 +1,92 @@
+"use luck";
+
+// tooltips text
+var tooltip_dynamic = 'هذه المسئلة ديناميكة'
+var tooltip_static = 'هذه المسئلة إستاتيكية'
+
+// icons
+const icon_search = `<svg width="24" height="24" viewBox="0 0 48 48" fill="rgba(22, 24, 35, 0.34)" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M22 10C15.3726 10 10 15.3726 10 22C10 28.6274 15.3726 34 22 34C28.6274 34 34 28.6274 34 22C34 15.3726 28.6274 10 22 10ZM6 22C6 13.1634 13.1634 6 22 6C30.8366 6 38 13.1634 38 22C38 25.6974 36.7458 29.1019 34.6397 31.8113L43.3809 40.5565C43.7712 40.947 43.7712 41.5801 43.3807 41.9705L41.9665 43.3847C41.5759 43.7753 40.9426 43.7752 40.5521 43.3846L31.8113 34.6397C29.1019 36.7458 25.6974 38 22 38C13.1634 38 6 30.8366 6 22Z"></path></svg>`
+const icon_book = `<svg width="25" height="25" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
+<path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+</svg>`
+
+tables_tanslation = {
+    "language": {
+        "sLengthMenu": "أظهر _MENU_ مدخلات",
+        "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
+        "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+        "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+        "sInfoPostFix": "",
+        "sSearch": "ابحث:",
+        "search": "_INPUT_"+`<div class='search-icon'>${icon_search}</div>`,
+        "sUrl": "",
+        "oPaginate": {
+            "sFirst": "الأول",
+            "sPrevious": "السابق",
+            "sNext": "التالي",
+            "sLast": "الأخير"
+        },
+        "zeroRecords": '88',
+        "emptyTable": 'لا توجد بيانات للعرض - قم بإضافة البعض من جدول المسائل'
+    },
+}
+const table1_args = {
+    "autoWidth":false,
+    "lengthChange":true,
+    "paging":true,
+    "pageLength": 25,
+    "lengthMenu": [ 25, 50, 100, 200, 800 ],
+    "bPaginate":false,
+    "order": [],
+    "bAutoWidth": false,
+    "bInfo":true,
+    "dom": '<"top"><f><t><"#margin5px"><"clear"><"footerContainer"lp>',
+    "rowReorder":false,
+    
+    ...tables_tanslation,
+
+    "columnDefs": [{
+        "searchable": true,
+        "orderable": false,
+        "targets": 0,
+        "className": "dt-center", 
+        "targets": "_all"
+    }], 
+
+    "createdRow": function(row, data, dataIndex){
+        // tooltip - table 1 - icons on hover tooltip.
+        $('.dynamic_lamp', row).hover(function(){
+            tippy(`.dynamic_lamp`, {
+                content: tooltip_dynamic,
+            });
+        });
+        $('.static_lamp', row).hover(function(){
+            tippy(`.static_lamp`, {
+                content: tooltip_static,
+            });
+        });
+    },
+}
+
+const table2_args = {
+    "rowReorder":true,
+    "lengthChange":false,
+    "bFilter": false,
+    "bLengthChange": false,
+    "bAutoWidth": false,
+    "bPaginate": false,
+    "bInfo":false,
+
+    ...tables_tanslation,
+
+    "columnDefs": [{
+        "searchable": true,
+        "orderable": false,
+        "targets": 0,
+        "className": "dt-center reorder", //row reorder - class 'reorder' changes cursor icon on hover.
+        "targets": "_all"
+    }],
+    
+}
+
+
