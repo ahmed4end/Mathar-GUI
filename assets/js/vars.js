c971037095dcfd11fa9b4681cde0b9608c4fd24b
ahmed4end <as4end@gmail.com>
2021-05-09 13:21:15 +0200

    patched table

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -12,7 +12,7 @@ const icon_book = `<svg width="25" height="25" fill="currentColor" class="bi bi-
 
 tables_tanslation = {
     "language": {
-        "sLengthMenu": "أظهر _MENU_ مدخلات",
+        "sLengthMenu": "مدخلات _MENU_ أظهر",
         "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
         "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
         "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
