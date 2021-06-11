0efaab5ac92b253644cb50263093895aba940786
ahmed4end <as4end@gmail.com>
2021-06-11 11:20:03 +0200

    about html update

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,42 +1,29 @@
 const about_html = `
-<p>هذه الإصدار من البرنامج يحتوى على<br>كتاب المعاصر الصف الأول الثانوى الترم الأول 2020 </p>
+<p>هذه الإصدار من البرنامج يحتوى على<br>كتاب المعاصر الصف الثالث الثانوى الترم الأول 2021 </p>
 <fieldset>
     <legend>
-    <h4>تواصل معنا</h4>
+        <h5>تواصل معنا</h5>
     </legend>
-    <div class="form-item">
-        <div class='form-item-left2'>     
-            <a onclick="eel.python_open_social('facebook')();" href="#"><img src='./assets/icons/fb.png'> Facebook</a>
-        </div>
-        <div class="form-item-middle">:</div>
-        <label id='HFCGDFGD' class="form-item-right"><small>(تواصل مع المطور)</small> فيسبوك</label>
-    </div>
-    <div class='line'></div>
-    <div class="form-item">
-        <div class="form-item-left2">
-            <a onclick="eel.python_open_social('whatsapp')()" href="#" style='color:green;' style='margin-right:auto;'><img src='./assets/icons/wt.png'> Whatsapp</a>
-        </div>
-        <div class="form-item-middle">:</div>
-        <label id='HFCGDFGD' class="form-item-right"><small>(تواصل مع المطور)</small> واتساب</label>
-    </div>
-    <div class='line'></div>
-    <div class="form-item">
-        <div class="form-item-left2">
-            <a onclick="eel.python_open_social('youtube')()" href="#" style='color:red;' style='margin-right:auto;'><img src='./assets/icons/yt.png'> Youtube</a>
-        </div>
-        <div class="form-item-middle">:</div>
-        <label id='HFCGDFGD' class="form-item-right">يوتيوب <small>(فديوهات شرح للبرنامج)</small></label>
+    <div class='grid' style='justify-content:space-evenly;'>
+        <a onclick="eel.python_open_social('facebook')();" href="#">
+            <img src='./assets/icons/fb.png'><span style='color:#3b5998'> Facebook</span>
+        </a>
+        <a onclick="eel.python_open_social('whatsapp')()" href="#" style='color:green;'>
+            <img src='./assets/icons/wt.png'><span> Whatsapp</span>
+        </a>
+        <a onclick="eel.python_open_social('youtube')()" href="#" style='color:red;'>
+            <img src='./assets/icons/yt.png'><span> Youtube</span>
+        </a>
     </div>
 </fieldset>
-<p>هذه الإصدار خاص بالأستاذ/<b>صلاح سرور</b></p>
-<small>© Ahmed Shokry</small>
 `
                
 // about mathar - modal
 $('.about-btn').on('click', function(){
     Swal.fire({
-        title: "<h1>Mathar 1.0.1</h1>",
-        html: about_html,
+        title  : "<h1 class='azure'>Mathar 1.1.0</h1>",
+        html   : about_html,
+        footer : "<span class='azure'>© Ahmed Shokry<span>",
         showConfirmButton: false,
     })
 });
