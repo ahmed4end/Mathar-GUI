21b66d495dd8fbfb5306af97b16079134fd07e72
ahmed4end <as4end@gmail.com>
2021-07-02 16:19:13 +0200

    about update

--- assets/js/about.js
+++ assets/js/about.js
@@ -6,13 +6,13 @@ const about_html = `
         <h5>تواصل معنا</h5>
     </legend>
     <div style='display:flex;padding:0.3em 0;justify-content:space-evenly;'>
-        <a onclick="eel.python_open_social('youtube')()" href="#" style='color:#FF0000;'>
+        <a onclick="eel.python_open_social('youtube')()" href="#" style='color:#FF0000;' title='فديوهات شرح البرنامج'>
             <img src='./assets/icons/yt.png'><span> Youtube</span>
         </a>
-        <a onclick="eel.python_open_social('whatsapp')()" href="#" style='color:#25D366;'>
+        <a onclick="eel.python_open_social('whatsapp')()" href="#" style='color:#25D366;' title='تواصل معى عبر واتساب'>
             <img src='./assets/icons/wt.png'><span> Whatsapp</span>
         </a>
-        <a onclick="eel.python_open_social('facebook')();" href="#">
+        <a onclick="eel.python_open_social('facebook')();" href="#" title='تواصل معى عبر فيسبوك'>
             <img src='./assets/icons/fb.png'><span style='color:#3b5998'> Facebook</span>
         </a>
     </div>
@@ -22,7 +22,7 @@ const about_html = `
 // about mathar - modal
 $('#about').on('click', function(){
     Swal.fire({
-        title  : "<h1 class='azure'>Mathar 1.1.0</h1>",
+        title  : "<h1 class='azure' title='Mather = Math(Mathematics) + Ar(Arabic)'>Mathar 1.1.0</h1>",
         html   : about_html,
         footer : modal_footer,
         showConfirmButton: false,
